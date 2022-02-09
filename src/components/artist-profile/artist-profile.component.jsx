696aefe2dd7bdcec60206d630b1d2ecb0004136b
Michael Chan <mikechansas@gmail.com>
2022-02-09 15:01:45 -0800

    Add artist info to Artist Profile component.

--- src/components/artist-profile/artist-profile.component.jsx
+++ src/components/artist-profile/artist-profile.component.jsx
@@ -1,15 +1,29 @@
+
 const ArtistProfile = ({
     artist,
-    artist: { name, id, images, genres, popularity, followers }
+    artist: { name, images, genres, popularity, followers }
 }) => {
-
-    console.log(artist);
 
     return (
         <div className='profile-container'>
-            <h1>{name}</h1>
-            <img src={images[0].url}
+            <h1 className='profile-name'>{name}</h1>
+            <img
+                className='profile-image'
+                src={images[0].url}
             />
+            <h2 className="profile-followers">
+                {`${followers.total} followers`}
+            </h2>
+            <h2>{`Artist Rank: ${popularity}`}</h2>
+            <div className="profile-genres">
+                {
+                    genres.map((genre, index) => {
+                        return (
+                            <span key={index}>{genre}</span>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 
