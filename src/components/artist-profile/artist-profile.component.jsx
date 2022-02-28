a5d1abcbdcad694fa841dad06da896a0241e4985
Michael Chan <mikechansas@gmail.com>
2022-02-28 12:00:58 -0800

    Restructure divs in Artist Profile Component.

--- src/components/artist-profile/artist-profile.component.jsx
+++ src/components/artist-profile/artist-profile.component.jsx
@@ -1,3 +1,4 @@
+import './artist-profile.styles.scss';
 
 const ArtistProfile = ({
     artist,
@@ -6,27 +7,32 @@ const ArtistProfile = ({
 
     return (
         <div className='profile-container'>
-            <h1 className='profile-name'>{name}</h1>
             <img
                 className='profile-image'
                 src={images[0].url}
             />
-            <h2 className="profile-followers">
-                {`${followers.total} followers`}
-            </h2>
-            <h2>{`Artist Rank: ${popularity}`}</h2>
-            <div className="profile-genres">
-                {
-                    genres.map((genre, index) => {
-                        return (
-                            <span key={index}>{genre}</span>
-                        )
-                    })
-                }
+            <div className='profile-info'>
+                <h1 className='profile-name'>{name}
+                    <h2 className="profile-popularity">
+                        {`Popularity Ranking: ${popularity}`}
+                    </h2>
+                    <h2 className="profile-followers">
+                        {`${followers.total.toLocaleString()} followers`}
+                    </h2>
+                </h1>
+                <div className="profile-genres">
+                    {
+                        genres.map((genre, index) => { //Add ampersand to string in case we are at last array element 
+                            genre = genre !== artist.genres[artist.genres.length - 1] ? ` ${genre}, ` : `& ${genre}`;
+                            return (
+                                <span key={index}>{genre}</span>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
     );
-
 }
 
 export default ArtistProfile
