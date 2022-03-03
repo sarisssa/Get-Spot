d18d21433b5c82be4754cd6c4ee85a774ffa18d1
Michael Chan <mikechansas@gmail.com>
2022-03-02 18:05:24 -0800

    Move Play and Pause SVG Icons to new assets folder from Tracks Container Component.

--- src/components/tracks-container/tracks-container.component.jsx
+++ src/components/tracks-container/tracks-container.component.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 
+import { ReactComponent as PlayIcon } from '../../assets/play-icon.svg';
+import { ReactComponent as PauseIcon } from '../../assets/pause-icon.svg';
+
 import './tracks-container.styles.scss';
 
 const TracksContainer = ({ tracks }) => {
@@ -67,8 +70,9 @@ const TracksContainer = ({ tracks }) => {
                             <div className="track-play">
                                 <div className="track-svg-container">
                                     {curSongURL === previewURL
-                                        ? <div><svg className="svg-inline--fa fa-pause fa-w-14" aria-hidden="true" data-prefix="fa" data-icon="pause" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M144 479H48c-26.5 0-48-21.5-48-48V79c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zm304-48V79c0-26.5-21.5-48-48-48h-96c-26.5 0-48 21.5-48 48v352c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48z"></path></svg></div>
-                                        : <div><svg className="svg-inline--fa fa-play fa-w-14" aria-hidden="true" data-prefix="fa" data-icon="play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path></svg></div>}
+                                        ? <PauseIcon />
+                                        : <PlayIcon />
+                                    }
                                 </div>
                             </div>
                             <p className="track-name">{track.name}</p>
